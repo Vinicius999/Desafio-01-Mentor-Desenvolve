b3c5eb2a160be8b08c79e90ec0279fa9febf2440
Vinicius999 <viniciussilva.dsc@gmail.com>
2023-04-06 16:55:29 -0300

    Feat: Database connection and table creation

--- main.py
+++ main.py
@@ -2,6 +2,22 @@ import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 from dotenv import load_dotenv
 import os
+import psycopg2
+
+
+def authentication():
+    load_dotenv()
+
+    CLIENT_ID = os.getenv('CLIENT_ID')
+    CLIENT_SECRET = os.getenv('CLIENT_SECRET')
+
+    auth_manager = SpotifyClientCredentials(
+        client_id = CLIENT_ID,
+        client_secret = CLIENT_SECRET
+    )
+
+    sp = spotipy.Spotify(auth_manager = auth_manager)
+    return sp
 
 
 def get_all_episodes_with_python(sp):
@@ -18,23 +34,47 @@ def get_all_episodes_with_python(sp):
             break  
     return episodes
 
-
-# Authentication 
-load_dotenv()
-
-CLIENT_ID = os.getenv('CLIENT_ID')
-CLIENT_SECRET = os.getenv('CLIENT_SECRET')
-
-auth_manager = SpotifyClientCredentials(
-    client_id = CLIENT_ID,
-    client_secret = CLIENT_SECRET
-)
-
-sp = spotipy.Spotify(auth_manager = auth_manager)
+class Database:
+    def __init__ (self):
+        print('Connecting to spotifydb...')
+        
+    def connect_db(self):
+        self.conn = psycopg2.connect(
+            host="localhost",
+            database="spotifydb",
+            user="postgres",
+            password="postgres"
+        )
+        return self.conn
+        
+        
+    def criate_db(self, sql):
+        self.conn = self.connect_db()
+        self.cur = self.conn.cursor()
+        self.cur.execute(sql)
+        self.conn.commit()
+        self.conn.close()   
+    
+   
+# Authentication
+sp = authentication()
 
 # Get episodes
 episodes = get_all_episodes_with_python(sp)
 
-for i, episode in enumerate(episodes):
-    print(f'{i} - {episode["name"][:100]}')
-    
+#for i, episode in enumerate(episodes):
+#    print(f'{i} - {episode["name"][:100]}')
+
+db = Database()
+
+sql = 'DROP TABLE IF EXISTS public.episodes'
+db.criate_db(sql)
+
+sql = '''
+    CREATE TABLE IF NOT EXISTS episodes (
+        id VARCHAR(50) PRIMARY KEY,
+        description TEXT,
+        link VARCHAR(255),
+        images VARCHAR(255)
+    )'''
+db.criate_db(sql)
