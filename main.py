8603c36b6930f9ae25e447ee479250d8ac423e30
Vinicius999 <viniciussilva.dsc@gmail.com>
2023-04-07 16:34:54 -0300

    Fix: Handling INSERT error, using 15920 15920 instead of ' '

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from spotipy.oauth2 import SpotifyClientCredentials
 from dotenv import load_dotenv
 import os
 import psycopg2
+import pandas as pd
 
 
 def authentication():
     load_dotenv()
-
     CLIENT_ID = os.getenv('CLIENT_ID')
     CLIENT_SECRET = os.getenv('CLIENT_SECRET')
 
@@ -40,13 +40,12 @@ class Database:
         
     def connect_db(self):
         self.conn = psycopg2.connect(
-            host="localhost",
-            database="spotifydb",
-            user="postgres",
-            password="postgres"
+            host='localhost',
+            database='spotifydb',
+            user='postgres',
+            password='postgres'
         )
         return self.conn
-        
         
     def criate_db(self, sql):
         self.conn = self.connect_db()
@@ -55,26 +54,49 @@ class Database:
         self.conn.commit()
         self.conn.close()   
     
-   
+    def insert_db(self, sql):
+        self.conn = self.connect_db()
+        self.cur = self.conn.cursor()
+        try:
+            self.cur.execute(sql)
+            self.conn.commit()
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(f"Error: {error}")
+            self.conn.rollback()
+            self.cur.close()
+            return 1
+        self.cur.close()
+        
+        
 # Authentication
 sp = authentication()
 
 # Get episodes
 episodes = get_all_episodes_with_python(sp)
+df = pd.DataFrame(episodes)
 
-#for i, episode in enumerate(episodes):
-#    print(f'{i} - {episode["name"][:100]}')
-
+# Database Class
 db = Database()
 
+# Creating table
 sql = 'DROP TABLE IF EXISTS public.episodes'
 db.criate_db(sql)
 
 sql = '''
     CREATE TABLE IF NOT EXISTS episodes (
-        id VARCHAR(50) PRIMARY KEY,
+        id VARCHAR PRIMARY KEY,
         description TEXT,
         link VARCHAR(255),
-        images VARCHAR(255)
-    )'''
+        images VARCHAR
+)'''
 db.criate_db(sql)
+
+# Inserting data in database
+for i in df.index:
+    sql = f"""
+        INSERT INTO episodes (id, description, link, images)
+        VALUES ($${df['id'][i]}$$, $${df['description'][i]}$$, $${df['external_urls'][i]}$$, $${df['images'][i]}$$);
+    """ #% (df['id'][i], df['description'][i], df['external_urls'][i], df['images'][i])
+    
+    db.insert_db(sql)
+    
