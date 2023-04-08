7d9248e4a6c404def3e96a19b2b2dd47dbf51224
Vinicius999 <viniciussilva.dsc@gmail.com>
2023-04-08 19:22:13 -0300

    Test: Test querying data

--- main.py
+++ main.py
@@ -136,7 +136,11 @@ for id in df['id']:
         """ 
         
         db.insert_db(sql)
-        
-        
 
+# Querying data
+sql = 'SELECT * FROM public.images'
+images = db.select_db(sql)
+
+# Tranformando os dados da consulta no PostegreSQL em DataFrame
+df_images = pd.DataFrame(images, columns=['id_episode', 'number', 'height', 'width', 'url'])
     
